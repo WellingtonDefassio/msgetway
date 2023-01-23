4a3c86975b67db77083a639150064bd428210c53
Wellington Defassio <well.defassio@gmail.com>
2023-01-22 21:18:09 -0300

    Initial commit

---
+++ src/main/java/io/git/welldefassio/msgetway/MsgetwayApplication.java
@@ -0,0 +1,13 @@
+package io.git.welldefassio.msgetway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgetwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsgetwayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/git/welldefassio/msgetway/MsgetwayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.git.welldefassio.msgetway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsgetwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
