aa1f8dc3b79e43bd086d49b6a0daa9d5563937f6
Wellington Defassio <well.defassio@gmail.com>
2023-01-22 21:18:32 -0300

    implementação do gateway clientes

--- src/test/java/io/git/welldefassio/msgetway/MsgetwayApplicationTests.java
+++ src/test/java/io/git/welldefassio/msgetway/MsgetwayApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MsgetwayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
